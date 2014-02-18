1d598a42a086ec350076a4c62e83fc074bbd65d9
pofider <honza.pofider@seznam.cz>
2014-02-18 12:24:32 +0100

    child templates

---
+++ extension/childTemplates/public/js/childTemplates.template.model.js
@@ -0,0 +1,18 @@
+﻿define(["app", "core/basicModel", "underscore"], function (app, ModelBase, _) {
+   
+    return ModelBase.extend({
+        
+        setTemplate: function (templateModel) {
+            this.templateModel = templateModel;
+            
+            this.set("isChildTemplate", templateModel.get("isChildTemplate"));
+        },
+
+        initialize: function () {
+            var self = this;
+            this.listenTo(this, "change", function() {
+                self.templateModel.set("isChildTemplate", self.get("isChildTemplate"));
+            });
+        },
+    });
+});
