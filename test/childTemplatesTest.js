d78ef941be07e3238e80895ac06c4be522034601
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2018-02-13 20:15:37 +0100

    rewrite to node 8

--- test/childTemplatesTest.js
+++ test/childTemplatesTest.js
@@ -1,138 +1,128 @@
 require('should')
-var path = require('path')
-var Reporter = require('jsreport-core').Reporter
+const jsreport = require('jsreport-core')
 
-describe('childTemplates', function () {
-  var reporter
+describe('childTemplates', () => {
+  let reporter
 
-  beforeEach(function () {
-    reporter = new Reporter({
-      rootDirectory: path.join(__dirname, '../')
-    })
+  beforeEach(() => {
+    reporter = jsreport({ tasks: { strategy: 'in-process' } })
+    reporter.use(require('../')())
+    reporter.use(require('jsreport-templates')())
+    reporter.use(require('jsreport-jsrender')())
 
     return reporter.init()
   })
 
-  it('should replace child template mark with its content', function () {
-    return reporter.documentStore.collection('templates').insert({
+  it('should replace child template mark with its content', async () => {
+    await reporter.documentStore.collection('templates').insert({
       content: 'xx',
       engine: 'jsrender',
       recipe: 'html',
       name: 't1'
-    }).then(function (t) {
-      var request = {
-        template: {content: '{#child t1}'},
-        options: {}
-      }
-
-      return reporter.childTemplates.evaluateChildTemplates(request, {}, true).then(function () {
-        request.template.content.should.be.eql('xx')
-      })
     })
+    const request = {
+      template: {content: '{#child t1}'},
+      options: {},
+      context: {}
+    }
+
+    await reporter.childTemplates.evaluateChildTemplates(request, {}, true)
+    request.template.content.should.be.eql('xx')
   })
 
-  it('should handle multiple templates in one', function () {
-    return reporter.documentStore.collection('templates').insert({
+  it('should handle multiple templates in one', async () => {
+    await reporter.documentStore.collection('templates').insert({
       content: '{{>~a()}}',
       engine: 'jsrender',
       helpers: 'function a() { return \'foo\'; }',
       recipe: 'html',
       name: 't1'
-    }).then(function (t) {
-      var request = {
-        template: {content: 'a{#child t1}ba{#child t1}'},
-        options: {}
-      }
-
-      return reporter.childTemplates.evaluateChildTemplates(request, {}, true).then(function () {
-        request.template.content.should.be.eql('afoobafoo')
-      })
     })
+    const request = {
+      template: {content: 'a{#child t1}ba{#child t1}'},
+      options: {},
+      context: {}
+    }
+
+    await reporter.childTemplates.evaluateChildTemplates(request, {}, true)
+    request.template.content.should.be.eql('afoobafoo')
   })
 
-  it('should handle multiple templates in nested one', function () {
-    return reporter.documentStore.collection('templates').insert({
+  it('should handle multiple templates in nested one', async () => {
+    await reporter.documentStore.collection('templates').insert({
       content: '{{>~a()}}',
       engine: 'jsrender',
       helpers: 'function a() { return \'foo\'; }',
       recipe: 'html',
       name: 't3'
-    }).then(function (t) {
-      return reporter.documentStore.collection('templates').insert({
-        content: '{#child t3}{#child t3}',
-        engine: 'jsrender',
-        recipe: 'html',
-        name: 't2'
-      }).then(function (t) {
-        var request = {
-          template: {content: '{#child t2}'},
-          options: {}
-        }
-
-        return reporter.childTemplates.evaluateChildTemplates(request, {}, true).then(function () {
-          request.template.content.should.be.eql('foofoo')
-        })
-      })
     })
+
+    await reporter.documentStore.collection('templates').insert({
+      content: '{#child t3}{#child t3}',
+      engine: 'jsrender',
+      recipe: 'html',
+      name: 't2'
+    })
+    const request = {
+      template: {content: '{#child t2}'},
+      options: {},
+      context: {}
+    }
+
+    await reporter.childTemplates.evaluateChildTemplates(request, {}, true)
+    request.template.content.should.be.eql('foofoo')
   })
 
-  it('should throw when there is circle in templates', function (done) {
-    reporter.documentStore.collection('templates').insert({
+  it('should throw when there is circle in templates', async () => {
+    await reporter.documentStore.collection('templates').insert({
       content: '{#child t2}',
       engine: 'jsrender',
       recipe: 'html',
       name: 't1'
-    }).then(function (t1) {
-      return reporter.documentStore.collection('templates').insert({
-        content: '{#child t1}',
-        engine: 'jsrender',
-        recipe: 'html',
-        name: 't2'
-      }).then(function (t2) {
-        return reporter.render({template: {shortid: t1.shortid}}, {}).then(function () {
-          done(new Error('Should throw'))
-        })
-      })
-    }).catch(function (e) {
-      e.weak.should.be.ok()
-      done()
     })
+    await reporter.documentStore.collection('templates').insert({
+      content: '{#child t1}',
+      engine: 'jsrender',
+      recipe: 'html',
+      name: 't2'
+    })
+
+    return reporter.render({template: {name: 't1'}}).should.be.rejected()
   })
 
-  it('should be able to pass data params to child', function () {
-    return reporter.documentStore.collection('templates').insert({
+  it('should be able to pass data params to child', async () => {
+    await reporter.documentStore.collection('templates').insert({
       content: '{{:foo}}',
       engine: 'jsrender',
       recipe: 'html',
       name: 't1'
-    }).then(function (t) {
-      var request = {
-        template: {content: '{#child t1 @data.foo=xx}'},
-        options: {}
-      }
-
-      return reporter.childTemplates.evaluateChildTemplates(request, {}, true).then(function () {
-        request.template.content.should.be.eql('xx')
-      })
     })
+
+    const request = {
+      template: {content: '{#child t1 @data.foo=xx}'},
+      options: {},
+      context: {}
+    }
+
+    await reporter.childTemplates.evaluateChildTemplates(request, {}, true)
+    request.template.content.should.be.eql('xx')
   })
 
-  it('should be able to pass data nested params to child', function () {
-    return reporter.documentStore.collection('templates').insert({
+  it('should be able to pass data nested params to child', async () => {
+    await reporter.documentStore.collection('templates').insert({
       content: '{{:foo.a}}',
       engine: 'jsrender',
       recipe: 'html',
       name: 't1'
-    }).then(function (t) {
-      var request = {
-        template: {content: '{#child t1 @data.foo.a=xx}'},
-        options: {}
-      }
-
-      return reporter.childTemplates.evaluateChildTemplates(request, {}, true).then(function () {
-        request.template.content.should.be.eql('xx')
-      })
     })
+    const request = {
+      template: {content: '{#child t1 @data.foo.a=xx}'},
+      options: {},
+      context: {}
+    }
+
+    await reporter.childTemplates.evaluateChildTemplates(request, {}, true)
+    request.template.content.should.be.eql('xx')
   })
 
   it.skip('should be able to pass stringified object as params', function () {
@@ -146,7 +136,8 @@ describe('childTemplates', function () {
         template: {
           content: '{#child t1 @data={foo: {"a": "hello"}}}'
         },
-        options: {}
+        options: {},
+        context: {}
       }
 
       return reporter.childTemplates.evaluateChildTemplates(request, {}, true).then(function () {
@@ -155,78 +146,74 @@ describe('childTemplates', function () {
     })
   })
 
-  it('should merge in params, not override', function () {
-    return reporter.documentStore.collection('templates').insert({
+  it('should merge in params, not override', async () => {
+    await reporter.documentStore.collection('templates').insert({
       content: '{{:main}}{{:foo}}',
       engine: 'jsrender',
       recipe: 'html',
       name: 't1'
-    }).then(function (t) {
-      var request = {
-        template: {content: '{#child t1 @data.foo=xx}'},
-        data: { main: 'main' },
-        options: {}
-      }
-
-      return reporter.childTemplates.evaluateChildTemplates(request, {}, true).then(function () {
-        request.template.content.should.be.eql('mainxx')
-      })
     })
+    const request = {
+      template: {content: '{#child t1 @data.foo=xx}'},
+      data: { main: 'main' },
+      options: {},
+      context: {}
+    }
+
+    await reporter.childTemplates.evaluateChildTemplates(request, {}, true)
+    request.template.content.should.be.eql('mainxx')
   })
 
-  it('should work with multiple data params', function () {
-    return reporter.documentStore.collection('templates').insert({
+  it('should work with multiple data params', async () => {
+    await reporter.documentStore.collection('templates').insert({
       content: '{{:a}}{{:b}}',
       engine: 'jsrender',
       recipe: 'html',
       name: 't1'
-    }).then(function (t) {
-      var request = {
-        template: {content: '{#child t1 @data.a=A @data.b=B}'},
-        data: {},
-        options: {}
-      }
-
-      return reporter.childTemplates.evaluateChildTemplates(request, {}, true).then(function () {
-        request.template.content.should.be.eql('AB')
-      })
     })
+    const request = {
+      template: {content: '{#child t1 @data.a=A @data.b=B}'},
+      data: {},
+      options: {},
+      context: {}
+    }
+
+    await reporter.childTemplates.evaluateChildTemplates(request, {}, true)
+    request.template.content.should.be.eql('AB')
   })
 
-  it('should be able to override template properties with params', function () {
-    return reporter.documentStore.collection('templates').insert({
+  it('should be able to override template properties with params', async () => {
+    await reporter.documentStore.collection('templates').insert({
       content: 'aaa',
       engine: 'jsrender',
       recipe: 'html',
       name: 't1'
-    }).then(function (t) {
-      var request = {
-        template: {content: '{#child t1 @template.content=xx}'},
-        options: {}
-      }
-
-      return reporter.childTemplates.evaluateChildTemplates(request, {}, true).then(function () {
-        request.template.content.should.be.eql('xx')
-      })
     })
+    const request = {
+      template: {content: '{#child t1 @template.content=xx}'},
+      options: {},
+      context: {}
+    }
+
+    await reporter.childTemplates.evaluateChildTemplates(request, {}, true)
+    request.template.content.should.be.eql('xx')
   })
 
-  it('should clone input data passed to child request', function () {
-    return reporter.documentStore.collection('templates').insert({
+  it('should clone input data passed to child request', async () => {
+    await reporter.documentStore.collection('templates').insert({
       content: '{{:a}}',
       engine: 'jsrender',
       recipe: 'html',
       name: 't1'
-    }).then(function (t) {
-      var request = {
-        template: {content: '{#child t1 @data.a=1}{#child t1 @data.a=2}'},
-        data: {},
-        options: {}
-      }
-
-      return reporter.childTemplates.evaluateChildTemplates(request, {}, true).then(function () {
-        request.template.content.should.be.eql('12')
-      })
     })
+    const request = {
+      template: {content: '{#child t1 @data.a=1}{#child t1 @data.a=2}'},
+      data: {},
+      options: {},
+      context: {}
+    }
+
+    await reporter.childTemplates.evaluateChildTemplates(request, {}, true)
+    request.template.content.should.be.eql('12')
   })
 })
