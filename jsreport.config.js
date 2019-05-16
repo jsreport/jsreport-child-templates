d57ab5cb729c4bf84909b632fa5982652810470b
BJR Matos <bjrmatos@gmail.com>
2019-05-16 13:06:39 -0500

    add parallelLimit option to better control processing of child templates
    fix jsreport/jsreport#546

--- jsreport.config.js
+++ jsreport.config.js
@@ -3,5 +3,19 @@ module.exports = {
   'name': 'child-templates',
   'main': 'lib/childTemplates.js',
   'dependencies': ['templates'],
-  'hasPublicPart': false
+  'hasPublicPart': false,
+  'optionsSchema': {
+    extensions: {
+      'child-templates': {
+        type: 'object',
+        properties: {
+          parallelLimit: {
+            type: 'number',
+            default: 2,
+            description: 'specifies the number of child templates that can be rendered in parallel at a time'
+          }
+        }
+      }
+    }
+  }
 }
